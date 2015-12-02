8bf1efa72045d03f8e29667707a3d9c797f177aa
Michelle Chen <mc4514@nyu.edu>
2015-12-01 21:33:29 -0500

    messed up

--- no_more_hangry/no_more_hangry/models/order.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var Order = new mongoose.Schema({
-    user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-    foodItem: {type: mongoose.Schema.Types.ObjectId, ref:'FoodItem'}
-});
-
-module.exports = mongoose.model('Order', Order);
--- no_more_hangry/no_more_hangry/models/restaurant.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var Restaurant = new mongoose.Schema({
-    name: {type: String, required: false},
-    zipCode: {type: Number, required: true}
-});
-
-module.exports = mongoose.model('Restaurant', Restaurant);
