ce3f98b89612a91874519fef0b2bf37411b4d6cf
Michelle Chen <mc4514@nyu.edu>
2015-12-04 15:28:39 -0500

    changed por

--- no_more_hangry/app.js
+++ no_more_hangry/app.js
@@ -55,7 +55,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://localhost/hangrydb');
+mongoose.connect('mongodb://localhost:10111/hangrydb');
 
 app.use(function(req, res, next){
   res.locals.user = req.user;
